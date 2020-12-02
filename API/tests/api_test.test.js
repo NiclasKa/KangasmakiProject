d34d4a77f0a3ec5bcb96f3bc2dc291a171e02aa1
Niclas.Kangasmaki <Niclas.Kangasmaki@codemen.fi>
2020-12-02 21:39:55 +0200

    More tests

--- API/tests/api_test.test.js
+++ API/tests/api_test.test.js
@@ -1,26 +1,34 @@
 const app = require('../app') // Link to your server file
 const supertest = require('supertest')
 const request = supertest(app)
+const axios = require('axios');
 
 // TODO: put state and get messages from httpserv and compare them with received messages
 
 it('get messages', async done => {
    const messages = await request.get('/messages');
-   const messagesFromApi = await request.get('http://localhost:8080/');
+   const messagesFromApi = await axios.get('http://localhost:8080/')
 
    expect(messages.status).toBe(200);
-   expect(messages.body.data).toBe(messagesFromApi.body.data);
+   expect(messages.body.data).toBe(messagesFromApi.data.data);
    done();
  });
 
  it('get state', async done => {
-   const paused = { state: 'PAUSED' }
-   await request.post('/state').send(paused)
    const getState = await request.get('/state');
    expect(getState.status).toBe(200);
-   expect(getState.body.state).toBe(paused)
+   expect(getState.body.state).toBe('RUNNING')
    done();
  });
+
+ it('put state', async done => {
+  const paused = { state: 'PAUSED' }
+  await request.put('/state').send(paused)
+  const getState = await request.get('/state');
+  expect(getState.status).toBe(200);
+  expect(getState.body.state).toBe('PAUSED')
+  done();
+});
 
  it('get runlog', async done => {
    const runLog = await request.get('/run-log')
